a06509c5b8dad88e29d38bff09335e517eab019f
Connor Ricks <connor.ricks@gmail.com>
2024-05-19 16:43:52 -0400

    Define supported platforms.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,12 @@ import PackageDescription
 let package = Package(
     name: "swift-money",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .macCatalyst(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .visionOS(.v1),
     ],
     products: [
         .library(name: "Money", targets: ["Money"]),
